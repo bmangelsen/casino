60f875d063a53fff22adb518ba422c788fe05f57
Ben Mangelsen <bmangelsen@gmail.com>
2017-01-02 11:18:12 -0500

    Deleted png folder, updated svg method to avoid rendering full file every time, and adjusted views/styles for future implementation of another player

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,22 +1,5 @@
 module ApplicationHelper
   def svg(name)
-    file_path = "#{Rails.root}/app/assets/images/svg/#{name}.svg"
-    return File.read(file_path).html_safe if File.exists?(file_path)
-        '(not found)'
-    # tag(:object, data: file_path, type: "image/svg")
+    content_tag(:object, nil, data: asset_url("svg/#{name}.svg"), type: "image/svg+xml")
   end
 end
-
-
-# def svg(name)
-#   file_path = "#{Rails.root}/public/images/svg/#{name}.svg"
-#   tag(:object, data: file_path, type: "image/svg+xml" )
-#   return File.read(file_path).html_safe if File.exists?(file_path)
-#   '(not found)'
-# end
-
-# def svg(name)
-#   File.open("public/images/svg/#{name}.svg", "rb") do |file|
-#     raw file.read
-#   end
-# end
--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -7,7 +7,6 @@ module PlayersHelper
   end
 
   def display_card(card)
-    return "#{card[0]}_of_#{card[1]}"
-    # svg("#{card[0]}_of_#{card[1]}")
+    svg "#{card[0]}_of_#{card[1]}"
   end
 end
