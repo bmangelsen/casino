4a7b4054405c5f7720b51b72e456bfcc0bda6206
Ben Mangelsen <bmangelsen@gmail.com>
2017-01-09 17:20:07 -0500

    Updated devise initializer for new email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,11 +78,11 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   address:              'smtp.sendgrid.net',
-  port:                 587,
+  port:                 '587',
   domain:               'heroku.com',
   user_name:            ENV['SENDGRID_USERNAME'],
   password:             ENV['SENDGRID_PASSWORD'],
-  authentication:       'plain',
+  authentication:       :plain,
   enable_starttls_auto: true  }
 
   config.web_socket_server_url = "wss://casino-application.herokuapp.com/cable"
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'happyfuntimescasino@gmail.com'
+  config.mailer_sender = 'app61932758@heroku.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = UserMailer
